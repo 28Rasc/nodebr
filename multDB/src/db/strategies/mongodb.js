d6e2389b74654a5084c87eaab8113adff8ce455b
Raul Cavalcanti <28rasc@gmail.com>
2020-01-27 11:25:07 -0300

    Strategy Pattern - modulo 5

---
+++ multDB/src/db/strategies/mongodb.js
@@ -0,0 +1,11 @@
+const ICrud = require('./interfaces/interfaceCrud');
+class MongoDB extends ICrud {
+  constructor() {
+    super();
+  }
+  create(item) {
+    console.log('O item foi salvo no mongoDB');
+  }
+}
+
+module.exports = MongoDB;
---
+++ multDB/src/db/strategies/postgres.js
@@ -0,0 +1,11 @@
+const ICrud = require('./interfaces/interfaceCrud');
+class Postgres extends ICrud {
+  constructor() {
+    super();
+  }
+  create(item) {
+    console.log(' O item foi salvo no Postegres');
+  }
+}
+
+module.exports = Postgres;
